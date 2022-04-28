77554d3e4c0d0c7d95c51525f4edf4622cf2b266
HannaYakubchyk <hanna.yakubchyk2.20@gmail.com>
2022-04-28 20:54:54 +0300

    With my test

---
+++ src/test/java/com/stv/factory/factorypages/RegistrationPage.java
@@ -0,0 +1,14 @@
+package com.stv.factory.factorypages;
+
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class RegistrationPage extends FactoryPage {
+    @FindBy(className = "bem-checkout")
+    private WebElement registrationContainer;
+
+    public boolean isRegistrationContainerDisplayed(){
+        return registrationContainer.isDisplayed();
+    }
+}
